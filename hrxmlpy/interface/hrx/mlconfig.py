08f0ea9af5f72d17fec97692239df23dfaeddf4b
russellmyers <flipside@netspace.net.au>
2021-08-13 12:41:51 +1000

    Update mlconfig class to use constructor params in place of env vars (repsonsibility of calling functions to set env vars and pass them)

--- hrxmlpy/interface/hrx/mlconfig.py
+++ hrxmlpy/interface/hrx/mlconfig.py
@@ -18,19 +18,46 @@ class MLConfig:
     DEFAULT_MODELS_ENDPOINT = "/api/V2/model-info"
     DEFAULT_CUSTOMER_MODELS_ENDPOINT = "/api/V2/customer-models"
 
-    def __init__(self):
-        pass
+    def __init__(self, api_base_url, api_client_id, api_client_secret):
+        self.api_base_url = api_base_url
+        self.api_client_id = api_client_id
+        self.api_client_secret = api_client_secret
+
+        self.api_customer_id = MLConfig.DEFAULT_CUSTOMER_ID
+        self.api_company_id  = MLConfig.DEFAULT_COMPANY_ID
+        self.auth_endpoint   = MLConfig.DEFAULT_AUTH_ENDPOINT
+        self.ml_services_endpoint = MLConfig.DEFAULT_ML_SERVICES_ENDPOINT
+        self.models_endpoint = MLConfig.DEFAULT_MODELS_ENDPOINT
+        self.customer_models_endpoint = MLConfig.DEFAULT_CUSTOMER_MODELS_ENDPOINT
+
+
+    def override_api_customer_company(self, api_customer_id, api_company_id):
+        """ Optional: used to override api customer_id and company_id  used in auth tokens if rqd"""
+        self.api_customer_id = api_customer_id
+        self.api_company_id  = api_company_id
+
+    def override_default_endpoints(self, auth_endpoint=None, ml_services_endpoint=None, models_endpoint=None, customer_models_endpoint=None):
+        """ Optional: used to override default endpoints if rqd """
+        if auth_endpoint is not None:
+           self.auth_endpoint = auth_endpoint
+        if ml_services_endpoint is not None:
+            self.ml_services_endpoint = ml_services_endpoint
+        if models_endpoint is not None:
+            self.models_endpoint = models_endpoint
+        if customer_models_endpoint is not None:
+           self.customer_models_endpoint = customer_models_endpoint
+
 
     @property
     def auth_token_header(self):
         auth_token_header = None
         data = {
-            "clientId": os.environ["MLCONFIG_API_CLIENT_ID"],
-            "clientSecret": os.environ["MLCONFIG_API_CLIENT_SECRET"],
-            "customerId": os.environ.get("MLCONFIG_API_CUSTOMER_ID", MLConfig.DEFAULT_CUSTOMER_ID),
-            "companyId": os.environ.get("MLCONFIG_API_COMPANY_ID", MLConfig.DEFAULT_COMPANY_ID)
+            "clientId": self.api_client_id,
+            "clientSecret": self.api_client_secret,
+            "customerId": self.api_customer_id,
+            "companyId": self.api_company_id,
         }
-        url = os.environ["MLCONFIG_API_BASE_URL"] + os.environ.get("MLCONFIG_API_AUTH_ENDPOINT", MLConfig.DEFAULT_AUTH_ENDPOINT)
+        url = self.api_base_url + self.auth_endpoint
         try:
             response = requests.post(url, data=data, headers=None)
         except Exception as e:
@@ -44,25 +71,24 @@ class MLConfig:
             bearer = "Bearer " + token
             auth_token_header = {"Authorization": bearer}
         else:
-            logging.error(f'{__class__.__name__} - Auth error for clientId: {os.environ["MLCONFIG_API_CLIENT_ID"]}')
+            logging.error(f'{__class__.__name__} - Auth error for clientId: {self.api_client_id}')
         return auth_token_header
 
-    @staticmethod
-    def get(endpoint):
-        token = MLConfig().auth_token_header
+    def get(self, endpoint):
+        token = self.auth_token_header
         response_json_data = {}
         if token:
-            request_url = os.environ["MLCONFIG_API_BASE_URL"] + endpoint
+            request_url = self.api_base_url + endpoint
             response = requests.get(request_url, headers=token)
             if response.status_code == 200:
                 response_json_data = json.loads(response.text)
             else:
-                print(f"{__class__.__name__} - {response.status_code} error for endpoint: {endpoint}")
+                print(f"{__class__.__name__} - {response.status_code} error for endpoint: {request_url}")
         return response_json_data
 
-    @staticmethod
-    def add_ml_service_technical_details(ml_service_details):
-        ml_service_tech_details_all = MLConfig().get(os.environ.get("MLCONFIG_API_ML_SERVICES_ENDPOINT", MLConfig.DEFAULT_ML_SERVICES_ENDPOINT))
+
+    def add_ml_service_technical_details(self, ml_service_details):
+        ml_service_tech_details_all = self.get(self.ml_services_endpoint)
         for model_config_params in ml_service_details:
             ml_service_tech_details = [
                 x for x in ml_service_tech_details_all["items"] if x["code"] == model_config_params["mlService"]
@@ -70,34 +96,32 @@ class MLConfig:
             model_config_params["mlType"] = ml_service_tech_details["mlType"]
             model_config_params["learningAlgorithm"] = ml_service_tech_details["learningAlgorithm"]
 
-    @staticmethod
-    def get_ml_model_details(gcc, lcc, payroll_area, country):
-        token = MLConfig().auth_token_header
-        model_info_endpoint = os.environ.get("MLCONFIG_API_MODELS_ENDPOINT", MLConfig.DEFAULT_MODELS_ENDPOINT)
+    def get_ml_model_details(self, gcc, lcc, payroll_area, country):
+        token = self.auth_token_header
+        model_info_endpoint = self.models_endpoint
         response_json_data = {}
         if token:
-            request_url = os.environ["MLCONFIG_API_BASE_URL"] + model_info_endpoint
+            request_url = self.api_base_url + model_info_endpoint
             response = requests.get(
                 request_url, headers=token, params={"gcc": gcc, "lcc": lcc, "payrollArea": payroll_area}
             )
             if response.status_code == 200:
                 response_json_data = json.loads(response.text)
-                MLConfig.add_ml_service_technical_details(response_json_data)
+                self.add_ml_service_technical_details(response_json_data)
             else:
                 logging.error(
-                    f"{__class__.__name__} - {response.status_code} error for endpoint: {model_info_endpoint}"
+                    f"{__class__.__name__} - {response.status_code} error for endpoint: {request_url}"
                 )
         return response_json_data
 
-    @staticmethod
-    def get_all_customers_using_model(ml_service, model_code):
-        token = MLConfig().auth_token_header
-        customer_models_endpoint = os.environ.get("MLCONFIG_API_CUSTOMER_MODELS_ENDPOINT", MLConfig.DEFAULT_CUSTOMER_MODELS_ENDPOINT)
+    def get_all_customers_using_model(self, ml_service, model_code):
+        token = self.auth_token_header
+        customer_models_endpoint = self.customer_models_endpoint
         response_json_data = {}
 
         customers = []
         if token:
-            request_url = os.environ["MLCONFIG_API_BASE_URL"] + customer_models_endpoint
+            request_url = self.api_base_url + customer_models_endpoint
             response = requests.get(request_url, headers=token)
             if response.status_code == 200:
                 response_json_data = json.loads(response.text)
@@ -112,38 +136,36 @@ class MLConfig:
                         )
             else:
                 logging.error(
-                    f"{__class__.__name__} - {response.status_code} error for endpoint: {customer_models_endpoint}"
+                    f"{__class__.__name__} - {response.status_code} error for endpoint: {request_url}"
                 )
         return customers
 
-    @staticmethod
-    def get_ml_model_details_by_model(ml_service, model_code):
-        all_custs = MLConfig.get_all_customers_using_model(ml_service, model_code)
+    def get_ml_model_details_by_model(self, ml_service, model_code):
+        all_custs = self.get_all_customers_using_model(ml_service, model_code)
         if len(all_custs) == 0:
             return {}
 
         cust = all_custs[0]  # Use first customer to retrieve model info
-        model_details = MLConfig.get_ml_model_details(cust["gcc"], cust["lcc"], cust["payroll_areas"][0], "")
+        model_details = self.get_ml_model_details(cust["gcc"], cust["lcc"], cust["payroll_areas"][0], "")
         for model_config_params in model_details:
             if model_config_params["mlService"] == ml_service:
                 return model_config_params
 
         return {}
 
-    @staticmethod
-    def get_ml_service_details(ml_service_code):
-        # TODO: To be updated once the ml-services endpoint is amended
+    def get_ml_service_details(self, ml_service_code):
+        # TODO: To be updated once the ml-services endpoint is amended to allow for ml service codes, not just ids
         ml_service_id_map = {"PAD": 1, "TWV": 2}
-        token = MLConfig().auth_token_header
-        ml_services_endpoint = f"{os.environ.get('MLCONFIG_API_ML_SERVICES_ENDPOINT', MLConfig.DEFAULT_ML_SERVICES_ENDPOINT)}/{ml_service_id_map[ml_service_code]}"
+        token = self.auth_token_header
+        ml_services_endpoint = f"{self.ml_services_endpoint}/{ml_service_id_map[ml_service_code]}"
         if token:
-            request_url = os.environ["MLCONFIG_API_BASE_URL"] + ml_services_endpoint
+            request_url = self.api_base_url + ml_services_endpoint
             response = requests.get(request_url, headers=token)
             if response.status_code == 200:
                 response_json_data = json.loads(response.text)
             else:
                 logging.error(
-                    f"{__class__.__name__} - {response.status_code} error for endpoint: {ml_services_endpoint}"
+                    f"{__class__.__name__} - {response.status_code} error for endpoint: {request_url}"
                 )
         return response_json_data
 
@@ -173,10 +195,7 @@ class MLConfig:
 
 
 if __name__ == '__main__':
-    os.environ["MLCONFIG_API_CLIENT_ID"] = "d7947bf7-3240-435d-8a79-5b2fc05248c7"
-    os.environ["MLCONFIG_API_CLIENT_SECRET"] = "DaTbzvauLJyFqDjpmE5nD5Q7t887TBHdZozxxagBsvQsHePXawgpwoTHuWSAydLj"
-    os.environ["MLCONFIG_API_BASE_URL"] = "http://localhost:5000"
-    ml_config = MLConfig()
+    ml_config = MLConfig(api_base_url= "http://localhost:5000", api_client_id="d7947bf7-3240-435d-8a79-5b2fc05248c7", api_client_secret="DaTbzvauLJyFqDjpmE5nD5Q7t887TBHdZozxxagBsvQsHePXawgpwoTHuWSAydLj")
     token_header = ml_config.auth_token_header
     print(token_header)
 
